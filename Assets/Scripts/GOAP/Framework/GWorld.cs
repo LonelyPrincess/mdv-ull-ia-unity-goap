ac03dc2701f22de10be7d5f7b6bf01a85decf053
Sara Hernández Suárez <sara.her.su@gmail.com>
2023-07-08 17:20:31 +0100

    feat: add base scripts for goap framework

---
+++ Assets/Scripts/GOAP/Framework/GWorld.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GWorld
+{
+    private static readonly GWorld instance = new GWorld();
+    private static WorldStates world;
+    private static WorldResources resources;
+
+    static GWorld()
+    {
+        world = new WorldStates();
+        resources = new WorldResources();
+    }
+
+    private GWorld()
+    {
+    }
+
+    public static GWorld Instance
+    {
+        get { return instance; }
+    }
+
+    public WorldStates GetWorld()
+    {
+        return world;
+    }
+
+    public WorldResources GetSharedResources ()
+    {
+        return resources;
+    }
+}
---
+++ Assets/Scripts/GOAP/Framework/WorldResources.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldResources
+{
+    public Dictionary<string, Queue<GameObject>> resources;
+
+    public WorldResources ()
+    {
+        resources = new Dictionary<string, Queue<GameObject>>();
+    }
+
+    public void AddResource (string resType, GameObject res)
+    {
+        if (!resources.ContainsKey(resType)) {
+            resources.Add(resType, new Queue<GameObject>());
+        }
+
+        resources[resType].Enqueue(res);
+    }
+
+    public GameObject RemoveResource (string resType)
+    {
+        if (!resources.ContainsKey(resType) || resources[resType].Count == 0) {
+            return null;
+        }
+        return resources[resType].Dequeue();
+    }
+
+    public Dictionary<string, Queue<GameObject>> GetResources ()
+    {
+        return resources;
+    }
+}
