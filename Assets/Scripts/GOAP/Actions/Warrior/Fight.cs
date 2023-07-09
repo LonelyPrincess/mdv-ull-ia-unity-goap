69eec0b0854037feb84d0a14a0bb84820f25f3d9
Sara Hernández Suárez <sara.her.su@gmail.com>
2023-07-09 13:50:51 +0100

    feat: update attack logic

--- Assets/Scripts/GOAP/Actions/Warrior/Fight.cs
+++ Assets/Scripts/GOAP/Actions/Warrior/Fight.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class Fight : GAction
 {
     GameObject opponent;
-    bool isBattleLost = false;
-
     public override bool PrePerform()
     {
         GameObject currentArenaSlot = inventory.FindItemWithTag("Arena Slot");
@@ -49,16 +47,35 @@ public class Fight : GAction
         GWorld.Instance.GetWorld().ModifyState(WorldStateProps.AvailableArenaSlots, -1);
         inventory.AddItem(target);*/
 
+        InvokeRepeating("Attack", 0, 1);
+        Invoke("CeaseAttack", this.duration);
+
         return true;
+    }
+
+    void CeaseAttack () {
+        CancelInvoke("Attack");
+        Animator anim = GetComponent<Animator>();
+        anim.SetBool("isAttacking", false);
     }
 
     void Attack () {
         if (beliefs.GetState("defeated") != null) {
             Debug.LogWarning("FIGHT: " + this.name + " cannot fight anymore, they already lost to " + opponent.name);
+            CeaseAttack();
             return;
         }
 
         bool isDeathlyBlow = Random.Range(0, 100) > 80;
+        if (isDeathlyBlow) {
+            Debug.LogWarning("FIGHT: " + this.name + " defeated " + opponent.name);
+            beliefs.ModifyState("winBattle", 1);
+
+            // Update status of opponent for them to know they've been defeated
+            opponent.GetComponent<GAgent>().beliefs.ModifyState("defeated", 0);
+        } else {
+            Debug.Log("FIGHT: " + opponent.name + " avoided an attack from " + this.name);
+        }
     }
 
     public override bool PostPerform()
@@ -67,31 +84,26 @@ public class Fight : GAction
         anim.SetBool("isAttacking", false);
 
         Debug.LogWarning("FIGHT: " + this.name + " has fought " + opponent.name);
-        bool isDeathlyBlow = Random.Range(0, 100) > 80;
-        if (isDeathlyBlow) {
-            Debug.LogWarning("FIGHT: " + this.name + " defeated " + opponent.name);
-            anim.SetBool("isAttacking", false);
-            beliefs.RemoveState("rested");
-            beliefs.ModifyState("exhausted", 0);
-            beliefs.ModifyState("winBattle", 1);
 
-            WorldResources resources = GWorld.Instance.GetSharedResources();
+        WorldResources resources = GWorld.Instance.GetSharedResources();
 
-            // Free arena slot
-            GameObject currentArenaSlot = inventory.FindItemWithTag("Arena Slot");
-            int arenaId = Mathf.Abs(currentArenaSlot.transform.parent.gameObject.GetInstanceID());
-            resources.AddResource(ResourceTypes.ArenaSlot, currentArenaSlot);
-            GWorld.Instance.GetWorld().ModifyState(WorldStateProps.AvailableArenaSlots, 1);
-            GWorld.Instance.GetWorld().ModifyState("fightersInArena" + arenaId, -2);
+        beliefs.RemoveState("rested");
+        inventory.RemoveItem(opponent);
 
-            // KO opponent
-            opponent.GetComponent<GAgent>().beliefs.RemoveState("rested");
-            opponent.GetComponent<GAgent>().beliefs.ModifyState("defeated", 0);
-            resources.AddResource(WorldStateProps.DefeatedWarriorsInArena, opponent);
+        // If battle was lost, add warrior to defeated pending to be picked up
+        if (beliefs.GetState("defeated") != null) {
+            resources.AddResource(WorldStateProps.DefeatedWarriorsInArena, this.gameObject);
             GWorld.Instance.GetWorld().ModifyState(WorldStateProps.DefeatedWarriorsInArena, 1);
-        } else {
-            Debug.Log("FIGHT: " + opponent.name + " avoided an attack from " + this.name);
+            return true;
         }
+
+        // If battle was won or ended up in draw, free arena slot
+        beliefs.ModifyState("exhausted", 0);
+        GameObject currentArenaSlot = inventory.FindItemWithTag("Arena Slot");
+        int arenaId = Mathf.Abs(currentArenaSlot.transform.parent.gameObject.GetInstanceID());
+        resources.AddResource(ResourceTypes.ArenaSlot, currentArenaSlot);
+        GWorld.Instance.GetWorld().ModifyState(WorldStateProps.AvailableArenaSlots, 1);
+        GWorld.Instance.GetWorld().ModifyState("fightersInArena" + arenaId, -1);
 
         return true;
     }
