692e3ac85abe129ae1dd6557ce1829cc818ecbe9
Sara Hernández Suárez <sara.her.su@gmail.com>
2023-07-09 14:04:31 +0100

    fix: avoid fighting self

--- Assets/Scripts/GOAP/Actions/Warrior/GoToArena.cs
+++ Assets/Scripts/GOAP/Actions/Warrior/GoToArena.cs
@@ -31,6 +31,7 @@ public class GoToArena : GAction
 
     public override bool PostPerform()
     {
+        GameObject self = this.gameObject;
         GameObject currentArenaSlot = inventory.FindItemWithTag("Arena Slot");
         int arenaId = Mathf.Abs(currentArenaSlot.transform.parent.gameObject.GetInstanceID());
         Debug.Log("FIGHT: " + this.name + " is going to arena " + arenaId);
@@ -38,13 +39,13 @@ public class GoToArena : GAction
 
         WorldResources resources = GWorld.Instance.GetSharedResources();
         GameObject opponent = resources.RemoveResource("fightersWaitingInArena" + arenaId);
-        if (opponent == null) {
+        if (opponent == null || opponent == self) {
             Debug.Log("FIGHT: " + this.name + " still has no opponent in arena " + arenaId);
-            resources.AddResource("fightersWaitingInArena" + arenaId, this.gameObject);
+            resources.AddResource("fightersWaitingInArena" + arenaId, self);
         } else {
             Debug.Log("FIGHT: " + this.name + " had an opponent waiting in arena " + arenaId);
             inventory.AddItem(opponent);
-            opponent.GetComponent<GAgent>().inventory.AddItem(this.gameObject);
+            opponent.GetComponent<GAgent>().inventory.AddItem(self);
         }
         return true;
     }
